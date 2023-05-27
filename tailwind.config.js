686fcd974602bf950354c8295dd355899e4f6cdd
Kristiyan Borisov <genkoph@protonmail.com>
2023-05-27 22:59:10 +0300

    feat: add primary color and sans font in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        primary: "#db5a2f"
+      }
     },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"]
+    }
   },
   plugins: [],
 }
