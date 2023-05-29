5fa566784933d4609434dfa7ee3c68b6e2bcba60
Kristiyan Borisov <genkoph@protonmail.com>
2023-05-29 11:52:56 +0300

    feat: implement blob

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import { Header } from "@/components";
 
 export default function Home() {
   return (
-    <main className="container px-3 py-6 mx-auto">
+    <main className="max-w-6xl px-3 py-6 mx-auto overflow-hidden">
       <Header />
 
-      <section className="flex mt-24 justify-between items-center">
-        <div className="max-w-[50%]">
+      <section className="flex mt-24 justify-between">
+        <div className="w-1/2">
           <h1 className="text-5xl font-semibold mb-8 leading-tight text-[#1f1f1f]">
             The Tastiest & <br />
             <span className="font-extrabold">Best Pizza</span> In The <br />
@@ -25,19 +25,33 @@ export default function Home() {
           </button>
         </div>
 
-        {/* TODO: add blob */}
-        <div className="right-0">
+        <div className="-mt-8 relative">
           <Image
             src="/pizza.png"
-            alt="pizza"
-            height={1096 / 2.6}
-            width={1011 / 2.6}
-            className="mr-8"
+            alt=""
+            width={1095 / 2.5}
+            height={1321 / 2.5}
+            className="mr-3"
           />
+
+          <svg viewBox="10.6 18 75.5 69.8" xmlns="http://www.w3.org/2000/svg" className="absolute -bottom-36 -right-36 -z-10 h-[450px] w-[450px]">
+            <path
+              d="M80.5,66.5Q69,83,47.5,87Q26,91,15.5,70.5Q5,50,17.5,32.5Q30,15,48,19Q66,23,79,36.5Q92,50,80.5,66.5Z"
+              stroke="none"
+              stroke-width="0"
+              className="fill-primary"
+            ></path>
+          </svg>
+          <path
+            d="M80.5,66.5Q69,83,47.5,87Q26,91,15.5,70.5Q5,50,17.5,32.5Q30,15,48,19Q66,23,79,36.5Q92,50,80.5,66.5Z"
+            stroke="none"
+            stroke-width="0"
+            fill="#4F46E5"
+          ></path>
         </div>
       </section>
 
-      <section className="flex mt-16 gap-6">
+      <section className="flex mt-14 gap-6">
         <div className="bg-[#e1fcc3] flex flex-col items-center p-4 rounded-2xl min-w-[160px] cursor-pointer [&>img]:hover:scale-110">
           <Image
             src="/images/pizza-1.png"
